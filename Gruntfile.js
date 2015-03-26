19097d2b5987c0fd7d57ef9badf2925155cedc11
Bob Wassermann <bob@wassermann.nl>
2015-03-26 14:41:35 +0100

    cordova init
    not fully functional yet

--- Gruntfile.js
+++ Gruntfile.js
@@ -204,6 +204,81 @@ module.exports = function (grunt) {
       }
     },
 
+    cordovacli: {
+      options: {
+        path: 'dist',
+        cli: 'cca'
+      },
+      cordova: {
+        options: {
+          command: ['create','platform','plugin','build'],
+          platforms: ['ios','android'],
+          plugins: ['device','dialogs'],
+          path: 'app_build',
+          id: 'me.bobwassermann.cnme',
+          name: 'CNME'
+        }
+      },
+      create: {
+        options: {
+            command: 'create',
+            id: 'nl.cnme',
+            name: 'CNME'
+        }
+      },
+      add_platforms: {
+        options: {
+            command: 'platform',
+            action: 'add',
+            platforms: ['ios', 'android']
+        }
+      },
+      add_plugins: {
+        options: {
+            command: 'plugin',
+            action: 'add',
+            plugins: [
+                'battery-status',
+                'camera',
+                'console',
+                'contacts',
+                'device',
+                'device-motion',
+                'device-orientation',
+                'dialogs',
+                'file',
+                'geolocation',
+                'globalization',
+                'inappbrowser',
+                'media',
+                'media-capture',
+                'network-information',
+                'splashscreen',
+                'vibration'
+            ]
+        }
+      },
+      build_ios: {
+        options: {
+            command: 'build',
+            platforms: ['ios']
+        }
+      },
+      build_android: {
+        options: {
+            command: 'build',
+            platforms: ['android']
+        }
+      },
+      emulate_android: {
+        options: {
+            command: 'emulate',
+            platforms: ['android'],
+            args: ['--target','Nexus5']
+        }
+      }
+    },
+
     // Compiles Sass to CSS and generates necessary files if requested
     compass: {
       options: {
