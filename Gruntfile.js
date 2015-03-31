8743e5d0fec7a4bfee2795feddcfcf92cef6a2b1
Bob Wassermann <bob@wassermann.nl>
2015-03-31 21:12:16 +0200

    cordova update

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,7 +18,7 @@ module.exports = function (grunt) {
   // Configurable paths for the application
   var appConfig = {
     app: require('./bower.json').appPath || 'app',
-    dist: 'dist'
+    dist: 'cordova/www'
   };
 
   // Define the configuration for all the tasks
@@ -201,81 +201,6 @@ module.exports = function (grunt) {
       sass: {
         src: ['<%= yeoman.app %>/styles/{,*/}*.{scss,sass}'],
         ignorePath: /(\.\.\/){1,2}bower_components\//
-      }
-    },
-
-    cordovacli: {
-      options: {
-        path: 'dist',
-        cli: 'cca'
-      },
-      cordova: {
-        options: {
-          command: ['create','platform','plugin','build'],
-          platforms: ['ios','android'],
-          plugins: ['device','dialogs'],
-          path: 'app_build',
-          id: 'me.bobwassermann.cnme',
-          name: 'CNME'
-        }
-      },
-      create: {
-        options: {
-            command: 'create',
-            id: 'nl.cnme',
-            name: 'CNME'
-        }
-      },
-      add_platforms: {
-        options: {
-            command: 'platform',
-            action: 'add',
-            platforms: ['ios', 'android']
-        }
-      },
-      add_plugins: {
-        options: {
-            command: 'plugin',
-            action: 'add',
-            plugins: [
-                'battery-status',
-                'camera',
-                'console',
-                'contacts',
-                'device',
-                'device-motion',
-                'device-orientation',
-                'dialogs',
-                'file',
-                'geolocation',
-                'globalization',
-                'inappbrowser',
-                'media',
-                'media-capture',
-                'network-information',
-                'splashscreen',
-                'vibration'
-            ]
-        }
-      },
-      build_ios: {
-        options: {
-            command: 'build',
-            platforms: ['ios']
-        }
-      },
-      build_android: {
-        options: {
-            command: 'build',
-            platforms: ['android']
-        }
-      },
-      emulate_android: {
-        options: {
-            command: 'emulate',
-            platforms: ['android'],
-            args: ['--target','Nexus5']
-        }
       }
     },
 
