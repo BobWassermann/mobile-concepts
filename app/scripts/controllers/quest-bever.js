0b98111704ab10162a0c3fd5375b2bf62972bd74
Bob Wassermann <bob@wassermann.nl>
2015-03-31 20:03:09 +0200

    directive for tekst-ballon 🙅🏿

--- app/scripts/controllers/quest-bever.js
+++ app/scripts/controllers/quest-bever.js
@@ -9,9 +9,26 @@
  */
 angular.module('mobileConceptsApp')
   .controller('QuestBeverCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
+    $scope.messages = [
+      'Hallo ik ben Bob de Bolle Bever. Kunnen jullie mij helpen met wat klusjes?',
       'AngularJS',
       'Karma'
     ];
-  });
+  }).directive('tekstBallon', function(){
+      return {
+        restrict: 'A',
+        scope: true,
+        template: '<ng-include src="getTemplateUrl()" />',
+        controller: function($scope) {
+          $scope.clicked = 0;
+          $scope.click = function(){
+            $scope.clicked++;
+          }
+          $scope.getTemplateUrl = function() {
+            if($scope.clicked <= ($scope.messages.length - 1)) {
+              return "/templates/bever-tekstballon.tpl.html"
+            }
+          }
+        }
+      }
+});
