25fd1342378ab335b5964b84bcc8dc7c5db2838b
Bob Wassermann <bob@wassermann.nl>
2015-03-31 21:48:52 +0200

    pijltjes

--- app/scripts/services/cordova.js
+++ app/scripts/services/cordova.js
@@ -1,21 +1,23 @@
-var d = $q.defer(),  
-resolved = false;
+'use strict';
 
-var self = this;  
-this.ready = d.promise;
+/**
+ * @ngdoc service
+ * @name mobileConceptsApp.cordova
+ * @description
+ * # cordova
+ * Factory in the mobileConceptsApp.
+ */
+angular.module('mobileConceptsApp')
+  .factory('cordova', function () {
+    // Service logic
+    // ...
 
-        document.addEventListener('deviceready', function () {
-    resolved = true;
-    d.resolve($window.cordova);
-});
+    var meaningOfLife = 42;
 
-// Check to make sure we didn't miss the
-// event (just in case)
-setTimeout(function () {  
-    if (!resolved) {
-        if ($window.cordova) d.resolve($window.cordova);
-    }
-}, 3000);
-
-// Public API here
-return this;
+    // Public API here
+    return {
+      someMethod: function () {
+        return meaningOfLife;
+      }
+    };
+  });
