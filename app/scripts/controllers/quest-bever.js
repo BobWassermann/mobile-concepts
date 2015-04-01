28bea3d9ac8939ee7590b4bd3d9e32ec8e139fbb
Bob Wassermann <bob@wassermann.nl>
2015-04-02 01:29:38 +0200

    multidimensional arrays

--- app/scripts/controllers/quest-bever.js
+++ app/scripts/controllers/quest-bever.js
@@ -10,9 +10,21 @@
 angular.module('mobileConceptsApp')
   .controller('QuestBeverCtrl', function ($scope, $state) {
     $scope.messages = [
-      'Hoi, ik ben Bob de Bolle Bever.',
-      'Ik speelde verstoppertje met mijn vriendjes, maar ze zijn er vandoor.',
-      'Kunnen jullie mij helpen mijn vriendjes terug te vinden?'
+      ['intro',
+        [
+          'Hoi, ik ben Bob de Bolle Bever.',
+          'Ik speelde verstoppertje met mijn vriendjes, maar ze zijn er vandoor.',
+          'Kunnen jullie mij helpen mijn vriendjes terug te vinden?'
+        ]
+      ],
+      ['kleinebeestjes',
+        [
+          'Ga op zoek naar kleine beestjes.',
+          'Ze verschuilen zich graag op vochtige plekjes.',
+          'Vang ze voorzichtig en bekijk ze heel goed!',
+          'Gebruik voor deze opdracht jullie loep.'
+        ]
+      ]
     ];
     $scope.opdrachten = [
       'Kleine beestjes',
@@ -26,7 +38,7 @@ angular.module('mobileConceptsApp')
     $scope.clicked = 0;
 
     $scope.click = function(){
-      var len = $scope.messages.length - 2;
+      var len = $scope.messages.length;
       if($scope.clicked <= len) {
         $scope.clicked++;
       } else {
