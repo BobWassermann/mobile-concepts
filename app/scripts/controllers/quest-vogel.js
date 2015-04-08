d2607740546c0c2693839dbc62955f3ae0ebe222
Jérôme Braeken <jeromebraeken@gmail.com>
2015-04-08 15:07:31 +0200

    🐝

--- app/scripts/controllers/quest-vogel.js
+++ app/scripts/controllers/quest-vogel.js
@@ -21,10 +21,7 @@ angular.module('mobileConceptsApp')
         $scope.clicked++;
       } else {
         document.querySelector('.tekstballon.vogel').style.display = 'none';
-        document.querySelector('.vogel').style.display = 'none';
+        document.querySelector('.vogel').classList.add('weg');      
       }
-
     };
-
-
 });
